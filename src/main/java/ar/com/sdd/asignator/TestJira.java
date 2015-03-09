77909b9ca67edf7d3a072ded1b4e1df00efdbb21
Emiliano <emi@Emilianos-iMac.local>
2015-03-09 00:16:38 -0300

    prueba jira rest. mejor

--- src/main/java/ar/com/sdd/asignator/TestJira.java
+++ src/main/java/ar/com/sdd/asignator/TestJira.java
@@ -1,23 +1,39 @@
 package ar.com.sdd.asignator;
 
-import java.net.URI;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
-import com.atlassian.jira.rest.client.JiraRestClient;
-import com.atlassian.jira.rest.client.NullProgressMonitor;
-import com.atlassian.jira.rest.client.domain.Issue;
-import com.atlassian.jira.rest.client.internal.jersey.JerseyJiraRestClientFactory;
+//http://www.mkyong.com/webservices/jax-rs/restfull-java-client-with-java-net-url/
+//Usar Gson con https://developer.atlassian.com/static/rest/jira/4.4.1.html#id150510
+
+
 
 public class TestJira {
 
 	public static void doit() {
 		try {
-	        final JerseyJiraRestClientFactory factory = new JerseyJiraRestClientFactory();
-	        final URI jiraServerUri = new URI("http://www.sdd.com.ar/jira");
-	        final JiraRestClient restClient = factory.createWithBasicHttpAuthentication(jiraServerUri, "eviera", "telperion");
-	        final NullProgressMonitor pm = new NullProgressMonitor();
-	        final Issue issue = restClient.getIssueClient().getIssue("ARSA-1", pm);
+			
+			URL url = new URL("http://www.sdd.com.ar/jira/rest/api/2.0.alpha1/issue/ARSA-1");
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("GET");
+			conn.setRequestProperty("Accept", "application/json");
 	 
-	        System.out.println(issue);
+			if (conn.getResponseCode() != 200) {
+				throw new RuntimeException("Failed : HTTP error code : " + conn.getResponseCode());
+			}
+	 
+			BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+	 
+			String output;
+			System.out.println("Output from Server .... \n");
+			while ((output = br.readLine()) != null) {
+				System.out.println(output);
+			}
+	 
+			conn.disconnect();			
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}		
